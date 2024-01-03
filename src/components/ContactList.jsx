f96c3abf879ed60dfae052d6b486d829c02f3d5b
Moises Rivera <morivera2015@gmail.com>
2024-01-02 23:29:39 -0500

    compd fetching data and useEffect

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,5 @@
 import ContactRow from "./ContactRow";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -9,6 +9,21 @@ const dummyContacts = [
 
 export default function ContactList() {
   const [contacts, setContacts] = useState(dummyContacts);
+
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
 
   console.log("Contacts:", contacts);
 
